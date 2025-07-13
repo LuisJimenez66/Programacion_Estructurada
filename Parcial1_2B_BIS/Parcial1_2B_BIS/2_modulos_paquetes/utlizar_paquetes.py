ba01073507f1c3afbfe4157bb0d289833b568b9a
LuisJimenez66 <luis_3141240113@utd.edu.mx>
2025-07-13 12:35:56 -0600

    Delete Parcial1_2B_BIS directory

--- Parcial1_2B_BIS/Parcial1_2B_BIS/2_modulos_paquetes/utlizar_paquetes.py
+++
@@ -1,9 +0,0 @@
-
-from paquete1 import modulos
-
-print(modulos.saludar("Saul"))
-modulos.esperar()
-modulos.borrar()
-nombre,telefono=modulos.datos2()
-print(f"\n\t..::: Agenda Telefonica:::...\n\t\tNombre: {nombre}\n\t\tTelefono: {telefono}")
-modulos.esperar()
